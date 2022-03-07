ea555bb799139da2cbc54424537ac95926c31740
Jonathan Henrique <jonathan.medeiros@picpay.com>
2022-03-07 10:18:37 -0300

    Adjusted private properties

--- components/tableRowElement.js
+++ components/tableRowElement.js
@@ -1,21 +1,21 @@
 export default class TableRowElement {
 
-    elementObject;
+    #elementObject;
 
     constructor (elementObject) {
-        this.elementObject = elementObject;
+        this.#elementObject = elementObject;
     }
 
     getLineWithData() {
         const line = document.createElement('tr');
         line.classList.add('table-line');
-        line.appendChild(this.elementObject.ageElement);
-        line.appendChild(this.elementObject.genderElement);
-        line.appendChild(this.elementObject.heightElement);
-        line.appendChild(this.elementObject.weightElement);
-        line.appendChild(this.elementObject.bmiElement);
-        line.appendChild(this.elementObject.classificationElement);
-        line.appendChild(this.elementObject.actionElement);
+        line.appendChild(this.#elementObject.ageElement);
+        line.appendChild(this.#elementObject.genderElement);
+        line.appendChild(this.#elementObject.heightElement);
+        line.appendChild(this.#elementObject.weightElement);
+        line.appendChild(this.#elementObject.bmiElement);
+        line.appendChild(this.#elementObject.classificationElement);
+        line.appendChild(this.#elementObject.actionElement);
         return line;
     }
 
--- domain/bmiRange.js
+++ domain/bmiRange.js
@@ -1,32 +1,32 @@
 export default class BmiRange {
-    bmi;
+    #bmi;
 
     constructor(bmi) {
-        this.bmi = bmi;
+        this.#bmi = bmi;
     }
 
     getBmiClassification() {
-        if (this.bmi >= 40) {
+        if (this.#bmi >= 40) {
             return 'Obesity Grade III';
         }
 
-        if (this.bmi >= 35 && this.bmi <= 39.9) {
+        if (this.#bmi >= 35 && this.#bmi <= 39.9) {
             return 'Obesity Grade II';
         }
 
-        if (this.bmi >= 30 && this.bmi <= 34.9) {
+        if (this.#bmi >= 30 && this.#bmi <= 34.9) {
             return 'Obesity Grade I';
         }
 
-        if (this.bmi >= 25 && this.bmi <= 29.9) {
+        if (this.#bmi >= 25 && this.#bmi <= 29.9) {
             return 'Slightly Overweight';
         }
 
-        if (this.bmi >= 18.6 && this.bmi <= 24.9) {
+        if (this.#bmi >= 18.6 && this.#bmi <= 24.9) {
             return 'Ideal Weight';
         }
 
-        if (this.bmi <= 18.5) {
+        if (this.#bmi <= 18.5) {
             return 'Under Weight';
         }
 
